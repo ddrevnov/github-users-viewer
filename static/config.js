ecd08cc232de4dd9984cc454bfd18fd9362b0cd4
krambertech <k.porshnieva@gmail.com>
2016-03-21 01:08:23 +0200

    Fix config

--- static/config.js
+++ static/config.js
@@ -1,4 +1,4 @@
 var APP_CONFIG = {
     numberOfProfilesToDisplay: 100,
-    githubApiToken: '_fdd83349eaddc6a102419f86dca3743fd1a542d7'
+    githubApiToken: 'fdd83349eaddc6a102419f86dca3743fd1a542d7'
 };
